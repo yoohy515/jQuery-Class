8735534e17e0fd8535381b18650d83f15a7fcbf9
yoohy515 <yoohy515@gmail.com>
2015-06-10 20:10:39 +0900

    RequireJS를 사용하여 main.js 비동기 호출

--- DEMO/js/main.js
+++ DEMO/js/main.js
@@ -1,23 +1,13 @@
-<!DOCTYPE html>
-<html lang="ko-KR">
-<head>
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
-	<meta charset="UTF-8">
-	<title>JS 모듈로더: requireJS</title>
-	<!-- <script src="js/libs/modernizr.custom.71961.js"></script> -->
-	<!-- <script src="js/libs/detectizr.min.js"></script> -->
-	<script src="js/require.js" data-main="js/main"></script>
-	<!-- <script src="js/libs/jquery-1.11.3.min.js"></script> -->
-</head>
-<body>
+/**
+ * --------------------------------
+ * Main Javascript File
+ * RequireJS를 사용해서 모듈을 호출하는 코드
+ *
+ * define()
+ * require()
+ * --------------------------------
+ */
 
-<script>
-	// 호출된 모듈 확인 코드
-	console.log(
-		'jQuery:' + window.jQuery+'\n',
-		'Modernizr:' + window.Modernizr+'\n',
-		'Detectizr:' + window.Detectizr
-	);
-</script>
-</body>
-</html>
+require(['libs/jquery-1.11.3.min'], function($) {
+	console.log($ === window.jQuery);
+});
