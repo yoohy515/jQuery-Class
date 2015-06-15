f0dcb322cdcd3622944985a59df5ab84103e9c43
yoohy515 <yoohy515@gmail.com>
2015-06-15 20:19:18 +0900

    DAY04

---
+++ www/build/build.css.js
@@ -0,0 +1,8 @@
+({
+    // build 디렉토리 기준 설정
+    cssIn       : "../css/style.css",
+    // build된 CSS 파일 출력 설정
+    out         : "../css/style.min.css",
+    // 최적화 설정
+    optimizeCss : "none", // "none", "standard.(keepLines,keepWhitespace,keepWhitespace)"
+})
