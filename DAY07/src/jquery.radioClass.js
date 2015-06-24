3433271aa5055b425e73768a268fb221f9d293e7
yoohy515 <yoohy515@gmail.com>
2015-06-24 18:54:59 +0900

    day07

--- DAY07/src/jquery.radioClass.js
+++ DAY07/src/jquery.radioClass.js
@@ -1,8 +1,6 @@
-// 전역이 오염되지 않도록 별도의 공간을 생성
-// 스코프 함수 (즉시 실행되는 함수 내부)
-(function(global, $){
+define(['jquery'], function($) {
 	'use strict';
-
+	
 	// $.fn.radioClass 플러그인이 존재하지 않는다면?
 	if ( !$.fn.radioClass ) {
 
@@ -48,5 +46,4 @@
 		};
 
 	}
-
-})(window, window.jQuery);
+});
